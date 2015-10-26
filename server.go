073da9331fb4e238e47bfa27780be3d155451caa
Larissa Voigt <hi@larissavoigt.com>
2015-10-26 14:02:42 -0400

    render embeded html

--- server.go
+++ server.go
@@ -1,10 +1,24 @@
 package main
-import "fmt"
-import "net/http"
+
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-  fmt.Println("Hello World!")
-  http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprint(res, "hello")
-} )
-http.ListenAndServe(":3000", nil)
+	var body string = `
+		<html>
+			<head>
+				<title>My Diary</title>
+			</head>
+			<body>
+				<h1>Hello World %s</h2>
+			</body>
+		</html>
+	`
+	fmt.Println("Hello World!")
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(res, body, req.URL.Query().Get("name"))
+	})
+	http.ListenAndServe(":3000", nil)
 }
