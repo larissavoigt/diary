fcfb7ce2984249f50aed185d79199a01a414cb76
Larissa Voigt <hi@larissavoigt.com>
2015-10-26 16:05:18 -0400

    Serve first assets

--- server.go
+++ server.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
-	"text/template"
 )
 
 type Page struct {
@@ -11,13 +11,19 @@ type Page struct {
 
 func main() {
 	var tpl = template.Must(template.ParseFiles("templates/index.html"))
+
+	fs := http.FileServer(http.Dir("assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
 	http.HandleFunc("/about", func(res http.ResponseWriter, req *http.Request) {
 		var page = &Page{"About"}
 		tpl.Execute(res, page)
 	})
+
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		var page = &Page{"My Diary"}
+		var page = &Page{"MyDiary"}
 		tpl.Execute(res, page)
 	})
+
 	http.ListenAndServe(":3000", nil)
 }
